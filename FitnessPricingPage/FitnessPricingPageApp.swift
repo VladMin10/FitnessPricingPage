0a7097e9d77c27593633e37b7e83a8665128b74b
Vladyslav Miniailo <miniailowladislav@gmail.com>
2025-03-06 18:02:07 +0200

    Initial Commit

---
+++ FitnessPricingPage/FitnessPricingPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessPricingPageApp.swift
+//  FitnessPricingPage
+//
+//  Created by Vladyslav Mi on 06.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessPricingPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
